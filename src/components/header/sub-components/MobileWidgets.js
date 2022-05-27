62949f078e0236522e658d57c3c0ae278df70257
Zubair Sheikh <izubairsh@gmail.com>
2022-05-27 21:55:24 +0500

    changes in mobile menu

--- src/components/header/sub-components/MobileWidgets.js
+++ src/components/header/sub-components/MobileWidgets.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 const MobileWidgets = () => {
   return (
@@ -7,12 +7,12 @@ const MobileWidgets = () => {
         <div className="header-contact-info">
           <ul className="header-contact-info__list">
             <li>
-              <i className="fa fa-phone"></i>{" "}
-              <a href="tel://12452456012">(1245) 2456 012 </a>
+              <i className="fa fa-phone"></i>{' '}
+              <a href="tel://+923044014345">+92 304 4014345</a>
             </li>
             <li>
-              <i className="fa fa-envelope"></i>{" "}
-              <a href="mailto:info@yourdomain.com">info@yourdomain.com</a>
+              <i className="fa fa-envelope"></i>{' '}
+              <a href="mailto:qadrimeat@gmail.com">qadrimeat@gmail.com</a>
             </li>
           </ul>
         </div>
